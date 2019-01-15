95c42bf9298d4d3c3eefb79a5486f9937e18ab49
Luna Lunapiena <lunacodes@gmail.com>
2019-01-15 12:42:08 -0500

    Increment to v 1.1.6 - switch to VVV environment

---
+++ plugins/luna-affiliates-widget/init.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Luna Affiliates Widget
+Plugin URI: https://lunacodesdesign.com
+Description: Display affiliate links in sidebar
+Author URI: https://lunacodesdesign.com
+Text Domain: luna_afl
+Version 1.0
+ */
+
+if ( file_exists( dirname( __FILE__ ) . '/luna-affiliates-widget.php') ) {
+    require_once dirname( __FILE__ ) . '/luna-affiliates-widget.php';
+}
+
+add_action( 'wp_enqueue_scripts', 'luna_afl_load_plugin_css' );
+function luna_afl_load_plugin_css() {
+    $plugin_url = plugin_dir_url( __FILE__ );
+
+    wp_enqueue_style( 'style', $plugin_url . 'assets/css/afl-style.css' );
+    // wp_enqueue_style( 'icons', $plugin_url . 'assets/css/fontello.css' );
+}
---
+++ themes/haSepharadi/front_page.php
@@ -0,0 +1,6 @@
+<?php
+
+// remove_action( 'genesis_before_post_content', 'genesis_post_meta' );
+// remove_action( 'genesis_after_post_content', 'genesis_post_meta' ); // replace another_hook with your desired hook
+
+// genesis();
