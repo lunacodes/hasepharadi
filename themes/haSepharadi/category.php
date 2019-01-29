58c2c40b6a24ccbcddbefecbf6eef924c2ce1f4e
Luna Lunapiena <lunacodes@gmail.com>
2019-01-28 20:04:29 -0500

    fix issues with category template. Need to clean up remove_actions in category.php

--- themes/haSepharadi/category.php
+++ themes/haSepharadi/category.php
@@ -9,11 +9,26 @@
  * @link    https://lunacodesdesign.com
  */
 
-// remove_action( 'genesis_before_content', 'custom_breadcrumbs', 8 );
-// remove_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
-// remove_action( 'genesis_loop', 'genesis_do_loop' );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
-// add_action( 'genesis_before_loop', 'haSepharadi_cat_header', 15 );
+remove_action( 'genesis_before_content', 'custom_breadcrumbs', 8 );
+remove_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+/* I haven't figured out which of these is actually responsible for removing the archive title yet*/
+
+remove_action( 'genesis_before_loop', 'genesis_do_date_archive_title' );
+remove_action( 'genesis_before_loop', 'genesis_do_author_title_description', 15 );
+
+
+remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_open', 5, 3 );
+
+remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_close', 15, 3 );
+
+remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_headline', 10, 3 );
+
+
+add_action( 'genesis_before_loop', 'haSepharadi_cat_header', 15 );
 function haSepharadi_cat_header() {
     $open = '<div class="archive-description taxonomy-archive-description taxonomy-description"><h1 class="archive-title"><span>';
     $title = single_cat_title( '', false );
@@ -21,7 +36,7 @@ function haSepharadi_cat_header() {
     echo($open . $title . $close);
 }
 
-// add_action( 'genesis_loop', 'luna_cat_loop' );
+add_action( 'genesis_loop', 'luna_cat_loop' );
 function luna_cat_loop() {
     if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
      <article class="post">
@@ -45,5 +60,7 @@ function luna_cat_loop() {
      wp_reset_postdata();
      endif;
 }
+
+
 
 genesis();
