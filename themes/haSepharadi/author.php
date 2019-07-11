472a34102f5a6ee710aac6ea1a5691100e3491ce
Luna <lunacodes@gmail.com>
2019-07-11 18:55:42 -0400

    Fix: Author pages broken. Make Author & Category code more DRY. Add Instagram
    _custom.scss
      Add Instagram Icon styles
    
    custom-functions.php
      Rewrite display_author_bio and haSepharadi_cat_header so they can be used by both Author and Category pages
    
    category.css
      Fix: content area not centered
    
    category.php
      Move add_action() statements to top of file
    
    author.php
      Move add_action() statements to top of file
      Fix: Affiliate Links code breaks site
      Use expanded format for "the loop", instead of concise format
      Move content header and author bio function to custom-functions.php
    
    Switched indentation to tabs in above-mentioned files.

--- themes/haSepharadi/author.php
+++ themes/haSepharadi/author.php
@@ -9,121 +9,108 @@
  * @link    https://lunacodesdesign.com
  */
 
-// remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
-
+// hasepharadi_cat_header() and display_author_bio
+// defined in custom-functions.php
 remove_action( 'genesis_before_content', 'custom_breadcrumbs', 8 );
 remove_action( 'genesis_loop', 'genesis_do_loop' );
-
 remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_headline', 10, 3 );
-
-add_action('wp_enqueue_scripts', 'enqueue_author_styles');
-function enqueue_author_styles() {
-		wp_enqueue_style('author.css', CHILD_URL . '/css/author.css', array(), CHILD_THEME_VERSION );
-}
-
+add_action( 'wp_enqueue_scripts', 'enqueue_author_styles' );
 add_action( 'genesis_before_loop', 'haSepharadi_cat_header' );
-function haSepharadi_cat_header() {
-	$open = '<div class="archive-description taxonomy-archive-description taxonomy-description"><h1 class="archive-title"><span>';
-	$title = get_the_author();
-	// echo($title);
-	$close = '</span></h1></div>';
-	echo($open . $title . $close);
-}
-
 add_action( 'genesis_loop', 'luna_cat_loop' );
-function luna_cat_loop() {
-	if ( have_posts() ): while ( have_posts() ) : the_post();
-		?>
-	 <article class="post">
-		<div class="thumbnail">
-			<a class="entry-image-link" href="<?php the_permalink(); ?>" aria-hidden="true"><?php echo(the_post_thumbnail( 'full' ) ); ?></a>
-		</div>
-
-		<span class="category"><a rel="category-tag"><?php the_category( ', ' ); ?></a></span>
-
-		 <!-- Display the Title as a link to the Post's permalink. -->
-		 <header class="entry-header"><h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink() ?>" class="entry-title-link" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2></header>
-		 <span class="title-divider"> </span>
-
-		<div class="entry-content" itemprop="text"> <?php the_excerpt(); ?> </div>
-
-		<footer class="entry-tools"> <span><?php the_date('F j, Y'); ?></span> <a href="https://hasepharadi.com/2018/03/23/peacefood-for-thought/" class="morelink">Continue to read <i class="fa fa-long-arrow-right"></i></a> </footer>
-	 </article> <!-- closes the first div box -->
+add_action( 'genesis_after_loop', 'display_affiliate_links' );
+add_action( 'genesis_after_loop', 'display_author_bio' );
 
 
-	 <?php
-	 // Setup for display_affiliate_links().
-	 $user_id = get_the_author_meta( 'ID' );
-
-	 // Define prefixed user ID
-	 $user_acf_prefix = 'user_';
-	 $user_id_prefixed = $user_acf_prefix . $user_id;
-
-		display_affiliate_links($user_id_prefixed);
-		endwhile;
-
-	 wp_reset_postdata($user_id_prefixed);
-	 endif;
+function enqueue_author_styles() {
+		wp_enqueue_style( 'author.css', CHILD_URL . '/css/author.css', array(), CHILD_THEME_VERSION );
 }
 
-function display_affiliate_links($user_id_prefixed) {
-	$user_id = get_the_author_meta( 'ID' );
-	$user_acf_prefix = 'user_';
-	$user_id_prefixed = $user_acf_prefix . $user_id;
-	?>
+function luna_cat_loop() {
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+				the_post();
+			?>
 
-	<div class="affiliates-container">
-	<?php
+		<article class="post">
+			<div class="thumbnail">
+				<a class="entry-image-link" href="<?php the_permalink(); ?>" aria-hidden="true"><?php echo( the_post_thumbnail( 'full' ) ); ?></a>
+			</div>
+
+			<span class="category"><a href="" rel="category-tag"><?php the_category( ',' ); ?></a></span>
+
+			<header class="entry-header">
+				<h2 class="entry-title" itemprop="headline">
+					<a href="<?php the_permalink(); ?>" class="entry-title-link" rel="bookmark" title="<?php the_title_attribute(); ?>" ></a>
+				</h2>
+			</header>
+			<span class="title-divider"></span>
+
+			<div class="entry-content" itemprop="text"><?php the_excerpt(); ?></div>
+
+			<footer class="entry-tools">
+				<span><?php the_date( 'F j, Y' ); ?></span>
+				<a href="<?php the_permalink(); ?>" class="morelink">Continue to read <i class="fa fa fa-long-arrow-right"></i></a>
+			</footer>
+
+		</article>
+
+				<?php
+				// Setup for display_affiliate_links().
+				$user_id = get_the_author_meta( 'ID' );
+
+				// Define prefixed user ID
+				$user_acf_prefix = 'user_';
+				$user_id_prefixed = $user_acf_prefix . $user_id;
+
+				display_affiliate_links( $user_id_prefixed );
+		}
+
+		wp_reset_postdata( $user_id_prefixed );
+	}
+}
+
+
+function display_affiliate_links( $user_id_prefixed ) {
+	$user_id = get_the_author_meta( 'ID' );
+	$user_acf_prefix = 'user';
+	$user_id_prefixed = $user_acf_prefix . $user_id;
+
+	echo( '<div class="affiliates-container">' );
 	if ( function_exists( 'have_rows' ) ) {
-		if ( have_rows( 'affiliate_links', $user_id_prefixed ) ) :
-			while ( have_rows( 'affiliate_links', $user_id_prefixed ) ) : the_row();
-				$count = count( have_rows( 'affiliate_links', $user_id_prefixed ) );
+		// echo( "have rows" );
+		if ( have_rows( 'affiliate_links' ) ) {
+			// echo( "have rows" );
+			while ( have_rows( 'affiliate_links' ) ) {
+				// echo( "have rows" );
+				the_row();
+				$count = count( hav_rows( 'affiliate_links', $user_id_prefixed ) );
 				echo( $count );
-				if ( 0 == ($count % 3) ) {
-					if ($count <= 3) {
-						$affiliate_class - 'affiliate-item';
-					} elseif ( $count > 3 ) {
-						$affiliate_class = 'affiliate-item row-fix';
-					}
+				if ( 0 === ( $count % 3 ) ) {
+					$affiliate_class - 'affiliate-item';
+				} elseif ($counte > 3 ) {
+					$affiliate_class = 'affiliate-item-row-fix';
 				} else {
 					$affiliate_class = 'affiliate-item';
 				}
 
 				$affiliate_image = get_sub_field( 'affiliate_image' );
-				$size = 'medium';
-				$thumb = $affiliate_image['sizes'][ $size ];
-				$width = $affiliate_image['sizes'][ $size . '-width' ];
-				$height = $affiliate_image['sizes'][ $size . '-height' ];
+				$size            = 'medium';
+				$thumb           = $affiliate_image['sizes'][ $size ];
+				$width           = $affiliate_image['sizes'][ $size . '-width' ];
+				$height          = $affiliate_image['sizes'][ $size . '-height' ];
+				$url             = $affiliate_image['url'];
+				$alt             = $affiliate_image['alt'];
 				?>
-			<div class="<?php echo $affiliate_class; ?>">
-				<img class="affiliate-img"src="<?php echo $affiliate_image['url']; ?>" alt="<?php echo $affiliate_image['alt']; ?>" width="<?php echo($width); ?>" height="<?php echo($height); ?>" />
-				<a class="affiliate-link-text" href="<?php the_sub_field( 'affiliate_url' ); ?>"><?php
-				the_sub_field( 'affiliate_link_text' ); ?></a>
-			</div>
 
-			<?php
-			endwhile;
-		endif;
-		// display_author_bio();
-
+				<div class="<?php echo $affiliate_class; ?>">
+					<img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>" class="affilate-image" width="<?php echo $width; ?>" height="<?php echo $height; ?>">
+					<a href="<?php the_sub_field( '$url' ); ?>" class="affiliate-link-text"><?php the_sub_field( 'affiliate_link_text' ); ?></a>
+				</div>
+				<?php
+			}
+		}
 	}
-
-	?>
-	</div>
-	<?php
-}
-
-add_action( 'genesis_after_loop', 'display_author_bio_2' );
-function display_author_bio_2() {
-
-	?>
-		<div class="author-box">
-			<!-- <h2 class="author-box-title">About Author</h2> -->
-			<div class="author-img"><a href="<?php echo( esc_url( get_author_posts_url( get_the_author_meta('ID') ) ) ) ?>" title="<?php esc_attr( get_the_author() ); ?>"><?php echo get_avatar(get_the_author_meta('user_email'), '80'); // Display the author gravatar image with the size of 80 ?></a></div>
-			<h3 class="author-name"><?php esc_html(the_author_meta('display_name')); // Displays the author name of the posts ?></h3>
-			<p class="author-description"><?php esc_textarea(the_author_meta('description')); // Displays the author description added in Biographical Info ?></p>
-		</div>
-	<?php
+	echo( '</div>' );
 }
 
 genesis();
--- themes/haSepharadi/category.php
+++ themes/haSepharadi/category.php
@@ -21,46 +21,40 @@ remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_heading
 remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_close', 15, 3 );
 remove_action( 'genesis_archive_title_descriptions', 'genesis_do_archive_headings_headline', 10, 3 );
 
-
 add_action('wp_enqueue_scripts', 'enqueue_category_scripts');
-function enqueue_category_scripts() {
-    if ( is_category() ) {
-        wp_enqueue_style('category.css', CHILD_URL . '/css/category.css', array(), CHILD_THEME_VERSION );
-        remove_action( 'genesis_before_entry', 'display_featured_post_image' );
-    }
-}
-
 add_action( 'genesis_before_loop', 'haSepharadi_cat_header', 15 );
-function haSepharadi_cat_header() {
-    $open = '<div class="archive-description taxonomy-archive-description taxonomy-description"><h1 class="archive-title"><span>';
-    $title = single_cat_title( '', false );
-    $close = '</span></h1></div>';
-    echo($open . $title . $close);
+add_action( 'genesis_loop', 'luna_cat_loop' );
+
+function enqueue_category_scripts() {
+	if ( is_category() ) {
+		wp_enqueue_style('category.css', CHILD_URL . '/css/category.css', array(), CHILD_THEME_VERSION );
+		remove_action( 'genesis_before_entry', 'display_featured_post_image' );
+	}
 }
 
-add_action( 'genesis_loop', 'luna_cat_loop' );
+
 function luna_cat_loop() {
-    if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
-     <article class="post">
-        <div class="thumbnail">
-            <a class="entry-image-link" href="<?php the_permalink(); ?>" aria-hidden="true"><?php echo(the_post_thumbnail( 'full' ) ); ?></a>
-        </div>
+	if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+		<article class="post">
+			<div class="thumbnail">
+				<a class="entry-image-link" href="<?php the_permalink(); ?>" aria-hidden="true"><?php echo(the_post_thumbnail( 'full' ) ); ?></a>
+			</div>
 
-        <span class="category"><a rel="category-tag"><?php the_category( ', ' ); ?></a></span>
+			<span class="category"><a rel="category-tag"><?php the_category( ', ' ); ?></a></span>
 
-         <!-- Display the Title as a link to the Post's permalink. -->
-         <header class="entry-header"><h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink() ?>" class="entry-title-link" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2></header>
-         <span class="title-divider"> </span>
+			<!-- Display the Title as a link to the Post's permalink. -->
+			<header class="entry-header"><h2 class="entry-title" itemprop="headline"><a href="<?php the_permalink() ?>" class="entry-title-link" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2></header>
+			<span class="title-divider"> </span>
 
-        <div class="entry-content" itemprop="text"> <?php the_excerpt(); ?> </div>
+			<div class="entry-content" itemprop="text"> <?php the_excerpt(); ?> </div>
 
-        <footer class="entry-tools"> <span><?php the_date('F j, Y'); ?></span> <a href="https://hasepharadi.com/2018/03/23/peacefood-for-thought/" class="morelink">Continue to read <i class="fa fa-long-arrow-right"></i></a> </footer>
-     </article> <!-- closes the first div box -->
+			<footer class="entry-tools"> <span><?php the_date('F j, Y'); ?></span><a href="<?php the_permalink(); ?>" class="morelink">Continue to read <i class="fa fa-long-arrow-right"></i></a> </footer>
+		</article> <!-- closes the first div box -->
 
 
-     <?php endwhile;
-     wp_reset_postdata();
-     endif;
+	<?php endwhile;
+	wp_reset_postdata();
+endif;
 }
 
 
